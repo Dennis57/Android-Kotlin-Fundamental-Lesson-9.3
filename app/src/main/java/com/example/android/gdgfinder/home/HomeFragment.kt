205d89ee2b0be01f93b0c3d645d85b6546633b8a
Dennis Chandrian <Dennischandrian@gmail.com>
2021-03-05 17:20:55 +0700

    Completed Lesson 10

--- app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -17,13 +17,21 @@
 
 package com.example.android.gdgfinder.home
 
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.android.gdgfinder.R
 import com.example.android.gdgfinder.databinding.HomeFragmentBinding
+import kotlinx.android.synthetic.main.add_gdg_fragment.*
 
 class HomeFragment : Fragment() {
 
@@ -40,6 +48,61 @@ class HomeFragment : Fragment() {
         val binding = HomeFragmentBinding.inflate(inflater)
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
+//        imageView.setImageBitmap(
+//            decodeSampledBitmapFromResource(resources, R.id.image, 100, 100)
+//        )
+        binding.viewModel = viewModel
+
+        viewModel.navigateToSearch.observe(viewLifecycleOwner,
+            Observer<Boolean> { navigate ->
+                if(navigate) {
+                    val navController = findNavController()
+                    navController.navigate(R.id.action_homeFragment_to_gdgListFragment)
+                    viewModel.onNavigatedToSearch()
+                }
+            })
+
         return binding.root
     }
+
+//    fun calculateInSampleSize(options: BitmapFactory.Options, reqWidth: Int, reqHeight: Int): Int {
+//        // Raw height and width of image
+//        val (height: Int, width: Int) = options.run { outHeight to outWidth }
+//        var inSampleSize = 1
+//
+//        if (height > reqHeight || width > reqWidth) {
+//
+//            val halfHeight: Int = height / 2
+//            val halfWidth: Int = width / 2
+//
+//            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+//            // height and width larger than the requested height and width.
+//            while (halfHeight / inSampleSize >= reqHeight && halfWidth / inSampleSize >= reqWidth) {
+//                inSampleSize *= 2
+//            }
+//        }
+//
+//        return inSampleSize
+//    }
+//
+//    fun decodeSampledBitmapFromResource(
+//        res: Resources,
+//        resId: Int,
+//        reqWidth: Int,
+//        reqHeight: Int
+//    ): Bitmap {
+//        // First decode with inJustDecodeBounds=true to check dimensions
+//        return BitmapFactory.Options().run {
+//            inJustDecodeBounds = true
+//            BitmapFactory.decodeResource(res, resId, this)
+//
+//            // Calculate inSampleSize
+//            inSampleSize = calculateInSampleSize(this, reqWidth, reqHeight)
+//
+//            // Decode bitmap with inSampleSize set
+//            inJustDecodeBounds = false
+//
+//            BitmapFactory.decodeResource(res, resId, this)
+//        }
+//    }
 }
